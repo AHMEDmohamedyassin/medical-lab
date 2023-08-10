90f44ffdf33950ca588d09c0ccc864127fe894cf
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2023-08-10 23:21:40 +0300

    v1

---
+++ app/components/header/MiddlePart.js
@@ -0,0 +1,39 @@
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const MiddlePart = () => {
+    const [menuState , setMenuState] = useState(false)
+  return (
+    <div className="">
+      <div className="max-w-[1170px] mx-auto flex justify-between items-center px-10 relative ">
+
+        {/* larg screen navbar */}
+        <nav className="hidden xl:flex justify-start items-center flex-1 text-main2 font-semibold gap-x-2">
+            <Link className="header_link relative pe-2 py-8 hover:text-main" href={'/'}>الصفحة الرئيسية</Link>
+            <Link className="header_link relative px-2 py-8 hover:text-main" href={'/'}>الخدمات</Link>
+            <Link className="header_link relative px-2 py-8 hover:text-main" href={'/'}>تواصل معنا</Link>
+            <Link className="header_link relative px-2 py-8 hover:text-main" href={'/'}>عنا</Link>
+        </nav>
+        {/* small screen menu button */}
+        <div className="flex-1 xl:hidden py-8 hover:cursor-pointer">
+          <span onClick={() => setMenuState(!menuState)} className="material-symbols-outlined text-gray-700 hover:cursor-pointer" style={{fontSize:40 , display:!menuState ? 'inline' : 'none'}}>menu</span>
+          <span onClick={() => setMenuState(!menuState)} className="material-symbols-outlined text-gray-700 hover:cursor-pointer" style={{fontSize:40 , display:menuState ? 'inline' : 'none'}}>close</span>
+        </div>
+        {/* logo div */}
+        <div className="flex-1 flex justify-end items-center">
+          <Link className="" href={"/"}>logo</Link>
+        </div>
+      </div>
+      {/* small screen menu */}
+      <div className="xl:hidden w-11/12 mx-auto flex flex-col justify-start items-start px-4 shadow-lg shadow-sky-100 border-main border-t-[3px]" style={{display:menuState?'flex' : 'none'}}>
+        <Link className="py-4 border-b-[1px] border-gray-200 w-full" href={'/'}>الصفحة الرئيسية</Link>
+        <Link className="py-4 border-b-[1px] border-gray-200 w-full" href={'/'}>الصفحة الرئيسية</Link>
+        <Link className="py-4 border-b-[1px] border-gray-200 w-full" href={'/'}>الصفحة الرئيسية</Link>
+        <Link className="py-4 border-b-[1px] border-gray-200 w-full" href={'/'}>الصفحة الرئيسية</Link>
+      </div>
+    </div>
+  )
+}
+
+export default MiddlePart
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import './animation.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +8,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ar" dir='rtl'>
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
